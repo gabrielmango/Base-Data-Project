6fd4921940c114705a1cd21cab82ffe1859fc8a7
gabrielhmango <gabrielhmango@gmail.com>
2025-07-09 14:04:47 -0300

    test(logging): add test_log_directory_and_file_creation

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,7 +1,10 @@
 import os
 import shutil
+from datetime import datetime
 
 import pytest
+
+from src.utils.logging import Logging
 
 
 @pytest.fixture
@@ -13,3 +16,12 @@ def cleanup_logs():
     yield
     if os.path.exists(log_dir):
         shutil.rmtree(log_dir)
+
+
+def test_log_directory_and_file_creation(cleanup_logs):
+    logger = Logging('teste_script.py')
+    expected_dir = f'logs/{datetime.now().strftime("%Y_%m_%d")}'
+    expected_file = f'{expected_dir}/teste_script.log'
+
+    assert os.path.isdir(expected_dir), 'Logs directory not created!'
+    assert os.path.isfile(expected_file), 'Log file not created!'
