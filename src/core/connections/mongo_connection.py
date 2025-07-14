36007337dabe1d4f6cb9632dd7a55432a975cce3
gabrielhmango <gabrielhmango@gmail.com>
2025-07-14 13:17:10 -0300

    refactor(MongoDBConnection): implement inheritance from BaseComponent

--- src/core/connections/mongo_connection.py
+++ src/core/connections/mongo_connection.py
@@ -1,18 +1,20 @@
 from pymongo import MongoClient
 
-from src.utils.logging import Logging
+from src.utils.base.base_component import BaseComponent
 
 
-class MongoDBConnection:
+class MongoDBConnection(BaseComponent):
     def __init__(self, connection_string, database_name):
-        self.logger = Logging(self.__class__.__name__)
+        super().__init__(name=self.__class__.__name__)
         self.connection_string = connection_string
         self.database_name = database_name
         self.client = None
         self.db = None
 
     def __enter__(self):
-        self.logger.info(f'Opening database connection: {self.database_name}')
+        self.logger.info(
+            f'Opening connection to MongoDB: {self.database_name}'
+        )
         self.client = MongoClient(self.connection_string)
         self.db = self.client[self.database_name]
         return self.db
@@ -20,4 +22,4 @@ class MongoDBConnection:
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.client:
             self.client.close()
-            self.logger.info('MongoDB connection closed.')
+            self.logger.info('Connection to MongoDB closed.')
